b4eb41a1629b76e172394ea5ea5629d166f3b93f
aa235553756 <aa235553756@gmail.com>
2022-08-24 15:14:12 +0800

    Update 2022-08-24T07:13:18.459Z

--- app/assets/js/all.js
+++
@@ -1,3 +0,0 @@
-$(".header__menu__link").on('click',function(){
-    $(".header__hamburger").toggleClass("sm-d-flex");
-})
---
+++ assets/js/all.js
@@ -0,0 +1,6 @@
+"use strict";
+
+$(".header__menu__link").on('click', function () {
+  $(".header__hamburger").toggleClass("sm-d-flex");
+});
+//# sourceMappingURL=all.js.map
